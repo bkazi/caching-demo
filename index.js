192f6ea678fbbe13fb72fd175d57f89f6c856b8f
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-05-07 14:34:01 +0100

    Remove api, use my pictures instead

--- index.js
+++ index.js
@@ -5,32 +5,20 @@ if ('serviceWorker' in navigator) {
 
 }
 
-const clientId = '57efb635002b823';
-let page = 0;
+const values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
 const grid = document.querySelector('.masonry-grid');
 const moreImages = document.querySelector('#more');
 let timeout;
 
 const addPictures = () => {
-    fetch(`https://api.imgur.com/3/g/memes/viral/${page}`, {
-        method: 'GET',
-        headers: {
-            'Authorization' : `Client-ID ${clientId}`,
-        }
-    })
-    .then((res) => res.json())
-    .then((json) => {
-        json.data.map((item) => {
-            const img = new Image();
-            img.onload = () => {
-                grid.appendChild(img);
-            };
-            img.src = item.link.replace(/^http:\/\//i, 'https://');
-        });
-        page++;
-    })
-    .catch(console.error);
+    values.map((item) => {
+        const img = new Image();
+        img.onload = () => {
+            grid.appendChild(img);
+        };
+        img.src = `/imgs/${item}-min.jpg`;
+    });
 };
 
 moreImages.addEventListener('click', addPictures);
